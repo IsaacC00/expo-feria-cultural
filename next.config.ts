48b44690630bd593ee7072eba9a2377f7b46c52f
Isaac <isaacpasquel1974@gmail.com>
2026-07-09 11:21:22 -0500

    mas cambios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/expo-feria-cultural',
   output: 'export',
   reactCompiler: true,
   images: {
