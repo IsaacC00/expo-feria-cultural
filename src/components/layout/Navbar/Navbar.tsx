3af8367ddf2c1d28529fa2bfa11d97fc116de5af
Isaac <isaacpasquel1974@gmail.com>
2026-07-10 18:52:24 -0500

    pagina stands corregido mas cambios mas cambios

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -133,7 +133,7 @@ export function Navbar() {
 
                         <Button>
 
-                            <a href="/exhibitors">
+                            <a href="/expo-feria-cultural/exhibitors">
                                 Reservar Stand
                             </a>
 
